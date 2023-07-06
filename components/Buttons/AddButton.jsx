8f1e3e2c738bbc87ba0d3818b6c86e88346f71a5
krish <kkrish7654@gmail.com>
2023-07-06 16:49:04 +0530

    all pages done

--- components/Buttons/AddButton.jsx
+++ components/Buttons/AddButton.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const AddButton = ({title}) => {
   return (
     <button className={`w-full h-[50px] text-[#666666] text-[16px] bg-white rounded-lg`}>{title}</button>
-
   )
 }
 
