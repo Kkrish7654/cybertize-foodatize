308209aa549c318768a6d37f40a9c51b48eb1707
krish <kkrish7654@gmail.com>
2023-07-10 12:31:49 +0530

    commit

--- app/demo/page.js
+++ app/demo/page.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Index = () => {
   return (
     <div>
-      <iframe src="/https://cybertize-foodatize-eecnz9pap-kkrish7654.vercel.app/" width="200" height="500" style="border:1px solid black;">
-      </iframe>
+      
     </div>
   )
 }
