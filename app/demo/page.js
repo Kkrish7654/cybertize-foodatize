d2af6cc292f141e045d12bebaa9bb7d985f19d9d
krish <kkrish7654@gmail.com>
2023-07-10 12:29:49 +0530

    merge

---
+++ app/demo/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>
+      <iframe src="/https://cybertize-foodatize-eecnz9pap-kkrish7654.vercel.app/" width="200" height="500" style="border:1px solid black;">
+      </iframe>
+    </div>
+  )
+}
+
+export default Index
