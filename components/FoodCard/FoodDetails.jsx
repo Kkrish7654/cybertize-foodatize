c07c304f90928829e666841244cbe3d7ea98e412
krish <kkrish7654@gmail.com>
2023-07-12 11:46:28 +0530

    popup

--- components/FoodCard/FoodDetails.jsx
+++ components/FoodCard/FoodDetails.jsx
@@ -9,7 +9,7 @@ const FoodDetails = ({item_name, item_description, isOpen, setIsOpen}) => {
   }
 
   return (
-    <div style={{boxShadow:'0 -5px 9999999px 999999px #0005'}} className={`fixed top-1/2 rounded-t-2xl w-full h-screen z-20 -m-5 bg-white ${isOpen ? 'block' : 'hidden'}`}>
+    <div style={{boxShadow:'0 9999px 0 999999px #0005'}} className={`fixed top-1/2 rounded-t-2xl w-full h-screen z-20 -m-5 bg-white ${isOpen ? 'block' : 'hidden'}`}>
       <div className='flex flex-col gap-2 p-4 w-[90%]'>
         <h4 className='text-lg font-semibold'>Chicken Chilli - Boneless</h4>
         <p>One plate Chicken Chilli Boneless with 12 pcs of chicken hot fried. Frshly brewed and Serverd</p>
